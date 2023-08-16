66397c1ba21c464ffab16875c7d7c3a10a7be4c4
luknda <lucasadnac@hotmail.com>
2023-08-16 11:26:14 -0300

    700

--- data_sample.R
+++ data_sample.R
@@ -1,3 +1,3 @@
 data = read.csv("data\\neo.csv")
 data_sample =  data[sample(nrow(data), size = 10000, replace = FALSE), ]
-data_sample_500 =  data[sample(nrow(data), size = 500, replace = FALSE), ]
+data_sample_700 =  data[sample(nrow(data), size = 700, replace = FALSE), ]
