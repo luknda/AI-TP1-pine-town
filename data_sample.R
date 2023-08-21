34a81e98a1ad895391c86643315e34aedba5caa7
Lucas Canda <lucasadnac@hotmail.com>
2023-08-21 12:08:18 -0300

    sample size change

--- data_sample.R
+++ data_sample.R
@@ -1,3 +1,3 @@
 data = read.csv("data\\neo.csv")
 data_sample =  data[sample(nrow(data), size = 10000, replace = FALSE), ]
-data_sample_700 =  data[sample(nrow(data), size = 700, replace = FALSE), ]
+data_sample_600 =  data[sample(nrow(data), size = 600, replace = FALSE), ]
